ff646fd57eae37362983a4e1ceb256d7bcc52705
duyguhz <duyghuhh@code.edu.az>
2022-06-01 23:21:15 +0400

    task added

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace AccessModifiers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            User user1 = new User();
+           
+               Console.WriteLine("Username-i daxil edin:");
+            string usernameStr = Console.ReadLine();
+
+
+            Console.WriteLine("Password-u daxil edin:");
+            string passwordStr = Console.ReadLine(); 
+        }
+
+        }
+    }
+
+
+
---
+++ User.cs
@@ -0,0 +1,67 @@
+﻿using System;
+namespace AccessModifiers
+{
+    public class User
+    {
+
+        private string _userName;
+        private string  _password;
+       
+
+        public string UserName
+        {
+            get => _userName;
+            set
+            {
+                if (value.Length >= 6 && value.Length <= 25)
+                    _userName = value;
+                    
+            }
+        }
+        public string Password
+        {
+            get => _password;
+            set
+            { 
+                if (value.Length >= 8 && value.Length <= 25)
+                {
+                    bool hasUpper = false;
+                    bool hasLower = false;
+                    bool hasDigit = false;
+                    for (int i = 0; i < value.Length; i++)
+                    {
+                        if (char.IsUpper(value[i]))
+                        
+                            hasUpper = true;
+                        
+                        if (char.IsLower(value[i]))
+                        
+                            hasLower = true;
+                        
+                        if (char.IsDigit(value[i]))
+                        
+                            hasDigit = true;
+
+                        
+                    }
+                    if (hasUpper && hasLower && hasDigit)
+                        _password = value;
+
+
+
+
+
+
+
+                    }
+                }
+                    
+                   
+                    
+
+
+                    
+
+            }
+        }
+    }
